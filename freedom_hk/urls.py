e8a6c85d94935dc02175e746db31cbc3cc415df2
Zharashan <116307474+zharashanus@users.noreply.github.com>
2024-11-10 03:29:18 +0500

    trying to add frontend

--- freedom_hk/urls.py
+++ freedom_hk/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='auth_freedom/home.html'), name='home'),
     path('', include('auth_freedom.urls')),
     path('recruiting/', include('recruiting_system.urls')),
     path('analyze/', include('analyze_candidates.urls')),
